41011672599abf22f66b755ddf961480876df7d3
Sebastian Mai <bastinat0r@bastinat0r.de>
2013-01-09 20:39:58 +0100

    adding possibility to start new worker

--- client.js
+++ client.js
@@ -11,12 +11,17 @@ if(!local)
 doRequest(20);
 doRequest(20);
 
+var sum = 0;
+var num = 0;
 function doRequest(n) {
 	var startTime = new Date();
 	var req = http.get(reqUrl + Math.floor(Math.random() * 10 + 5), function(res) {
 		res.on('data', function(data) {
 			var endTime = new Date();
-			util.puts((endTime.getTime() - startTime.getTime()) / 1000.0);
+			var time = (endTime.getTime() - startTime.getTime()) / 1000.0
+			sum += time;
+			num++;
+			util.puts("t: " + time + "  \tavg: " + sum / num );
 			if(n > 0)
 				doRequest(n - 1);
 		});
--- sheduler.js
+++ sheduler.js
@@ -3,6 +3,7 @@ var util = require('util');
 var url = require('url');
 var querystring = require('querystring');
 var events = require('events');
+var exec = require('child_process').exec;
 resultEmitter = new events.EventEmitter();
 
 var port = 3000;
@@ -11,8 +12,11 @@ var workers = [];
 var idCount = 0;
 
 var queue = [];
+var azure_vm_names = ['foobar'];
+
+var cacheing = false;
 var negativeCaching = false;
-var prefill = true;
+var prefill = false;
 
 if(prefill) {
 	for(var i = 0; i<15; i++) {
@@ -38,7 +42,7 @@ var srv = http.createServer(function(req, res) {
 				var worker = JSON.parse(data);
 				if(worker.result) {
 					resultEmitter.emit('result'+worker.result.id, worker.result);
-					if(worker.result.num >= 0 || negativeCaching) {
+					if(cacheing && (worker.result.num >= 0 || negativeCaching)) {
 						cache[worker.result.param] = worker.result.num;
 					}
 				}
@@ -59,6 +63,13 @@ var srv = http.createServer(function(req, res) {
 			res.writeHead(200);
 			res.end(JSON.stringify(workers.length - 1));
 		}
+		
+		if(/^\/load/.test(req.url)) {
+			worker = JSON.parse(data);
+			res.writeHead(200);
+			res.end(JSON.stringify(workers.length - 1));
+		}
+		
 		if(/^\/client/.test(req.url)) {
 			var reqUrl = url.parse(req.url);
 			var param = querystring.parse(reqUrl.query);
@@ -66,7 +77,7 @@ var srv = http.createServer(function(req, res) {
 				param : param.time,
 				id : idCount
 			};
-			if(cache[param.time]) {
+			if(cacheing && cache[param.time]) {
 				var answer = {
 					num : cache[param.time],
 					param : param.time,
@@ -97,10 +108,30 @@ function enqueJob(job) {
 		}
 	}
 	queue.push(job);
+	if(queue.length > 10) {
+		startWorker(azure_vm_names[0]);
+	}
 };
 
 function startJob(worker, job) {
 	var req = http.request(worker.opts, function(res) {
 	});
 	req.end(JSON.stringify(job));
+}
+
+function execCB(err, stdout, stderr) {
+		if(err) {
+			util.puts("Err: " + JSON.stringify(err));
+		}
+		util.puts(stdout);
+		util.puts(stderr);
+	});
+}
+
+function startWorker() {
+	var child = exec("azure vm start " + vmname, execCB);
+}
+
+function stopWorker() {
+	var child = exec("azure vm stop " + vmname, execCB);
 }
